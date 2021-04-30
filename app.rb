bcddb27ce121b7b99920d94503b270d220db452e
rupinderkaur30-gif <rupi063094@gmail.com>
2021-04-30 19:04:04 -0400

    commit tree 7b5e2886265b3a0fcd68c8f70c00ad3ec2e79d2f

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Rupi"
+    end
+
+    get '/hometown' do
+        " My hometown is queens"
+    end
+    get '/favorite-song' do
+        "My favorite song is Thank you"
+    end
 end
